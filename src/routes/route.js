40c05f5c26dd2aeb571b518df7e37731f345573f
kimmi kumari sinha <96224700+kimmi15@users.noreply.github.com>
2022-06-03 01:19:50 +0530

    updated file

--- src/routes/route.js
+++ src/routes/route.js
@@ -13,11 +13,84 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
-router.get('/hello', function (req, res) {
+router.get('/movies', function (req, res) {
+    var movies=["The Shawshank Redemption ",
+    "The Godfather",
+    " The Dark Knight",
+    " The Godfather: Part II",
+    " 12 Angry Men ",
+    " Schindler's List ",
+    "The Lord of the Rings: The Return of the King",
+    " Pulp Fiction",
+ ]
    
-    res.send('Hello there!')
+    res.send(movies)
+
 });
 
+
+router.get('/movies/:indexNumber', function (req, res) {
+    var movies=["The Shawshank Redemption ",
+    "The Godfather",
+    " The Dark Knight",
+    " The Godfather: Part II",
+    " 12 Angry Men ",
+    " Schindler's List ",
+    "The Lord of the Rings: The Return of the King",
+    " Pulp Fiction",
+ ]
+    // res.send(movies[req.params.indexNumber])
+    if(req.params.indexNumber < 0 || req.params.indexNumber > movies.length){
+     res.send("invalid option");
+    }
+    else{
+        res.send(movies[req.params.indexNumber])
+    }
+});
+
+
+router.get('/films', function (req, res) {
+    var store=[ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "ncendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name:": "Finding Nemo"
+       }
+    ]   
+    res.send(store)
+});
+router.get('/films/:filmId', function (req, res) {
+    var store=[ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "ncendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name:": "Finding Nemo"
+       }
+    ]   
+
+    if(req.params.filmId < 0 || req.params.filmId > store.length){
+        res.send("chala ja ya se ")
+    }else{
+       const singleData =  store.find(ele => ele.id ===  +req.params.filmId )
+    //    console.log(singleData)
+    res.send(singleData)
+    }
+   
+});
 router.get('/candidates', function(req, res){
     console.log('Query paramters for this request are '+JSON.stringify(req.query))
     let gender = req.query.gender
