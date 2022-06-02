d1aab04437db38e436f8e61fc6417283b1af0db4
kimmi kumari sinha <96224700+kimmi15@users.noreply.github.com>
2022-06-03 00:05:35 +0530

    update

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,11 @@
 const express = require('express');
 // const externalModule = require('./logger')
 
+const lodash = require('lodash')
 const logger = require('../logger/logger')
 const helper = require('../util/helper')
 const validator = require('../validator/formatter')
+
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
@@ -28,8 +30,24 @@ router.get('/test-me2', function (req, res) {
     res.send('My third api!')
 });
 
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
+router.get('/hello', function (req, res) {
+    let arr=["janbuary","febuary","March", "April", 'May', 'June', 'July', "Audgust", 'September', "October", "November","December"]
+    //  lodash.chunk(arr,3)//chunk is function //load dash package 
+     console.log(lodash.chunk(arr,3))
+     let odd=[3,5,7,9,23,45,29,41,21,25]
+     //tail function is returing all element by removing first elements
+    //  lodash.tail(odd)
+    console.log( lodash.tail(odd))
+    var new_arr=([2,3,4,5,6],[2,3,4,5,2],[903,12,3,56,67],[4546,7884,3,356,67],[89,34,57,78,22])
+    // lodash.union(new_arr)
+    console.log(lodash.union(new_arr))
+    const new_arr2=[
+        ["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]
+        ]
+    //lodash.fromPairs(new_arr2)
+    console.log(lodash.fromPairs(new_arr2));
+         res.send('My 4th api!')
+
 });
 
 router.get('/test-me4', function (req, res) {
