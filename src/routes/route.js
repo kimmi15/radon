a8f019c4d1dabd59f94436365d3dbedde5d6f017
kimmi kumari sinha <96224700+kimmi15@users.noreply.github.com>
2022-06-04 14:33:00 +0530

    add new

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+const players  = [
+    {
+      name: "manish",
+      dob: "1/1/1995",
+      gender: "male",
+      city: "jalandhar",
+      sports: ["swimming"],
+    },
+    {
+      name: "gopal",
+      dob: "1/09/1995",
+      gender: "male",
+      city: "delhi",
+      sports: ["soccer"],
+    },
+    {
+      name: "lokesh",
+      dob: "1/1/1990",
+      gender: "male",
+      city: "mumbai",
+      sports: ["soccer"],
+    },
+  ];
+  
 router.get('/students/:name', function(req, res) {
     let studentName = req.params.name
     console.log(studentName)
@@ -69,5 +93,27 @@ router.post("/test-post-4", function(req, res) {
     arr.push(ele)
     res.send(  { msg: arr , status: true }  )
 })
+
+
+router.post('/players', function (req, res) {
+    // const isPlayer =  players.find(ele => ele.name === req.body.name )
+    // if(!isPlayer){
+    //  players.push(req.body);
+    //  res.send({plyersData: players})
+    // }else{
+    //   res.send({succes: false , error : "That person already exists "})
+    // }
+    //LOGIC WILL COME HERE
+  const players_new=players.find((ele) => ele.name===req.body.name );
+    if(!players_new){
+        players.push(req.body);
+        res.send({players:players})
+    }
+    else{
+        res.send({succes:false,error:"That person alerady exists"})
+    }
+ 
+})
+
 
 module.exports = router;
